45653413f708e92020a8bc7f3c2b53016e6df22f
ValentinWitz <s1610237037@students.fh-hagenberg.at>
2018-01-11 00:34:09 +0100

    sc

--- WhoWouldWin/Location/LocationBattleViewController.swift
+++ WhoWouldWin/Location/LocationBattleViewController.swift
@@ -14,25 +14,53 @@ class LocationBattleViewController: UIViewController, CLLocationManagerDelegate 
     
     let manager = CLLocationManager()
     var ref: DatabaseReference?
+    var loc: DatabaseReference?
     var refHandle: DatabaseHandle?
     var locationRadius:Double = 10
     
     override func viewWillAppear(_ animated: Bool) {
         manager.delegate = self
-        //100 meters is the accurayc: possible changes -> 10m or bestpossible
         manager.desiredAccuracy = kCLLocationAccuracyHundredMeters
         manager.requestWhenInUseAuthorization()
         manager.startUpdatingLocation()
+        
+        ref = Database.database().reference()
+        
+        refHandle = ref?.child("Locations").observe(.value, with: { (snapshot) in
+            for location in snapshot.children{
+                print(location)
+            }
+        })
+        
+        //let childSnapshot = snapshot.childSnapshotForPath(child.key)
+        
+        
+//        let databaseHandle = databaseRef.observe(.value, with: { (snapshot) in
+//            for item in snapshot.children {
+//
+//                if let dbLocation = snapshot.childSnapshot(forPath: "LocationName") as? String {
+//
+//                    print (dbLocation)
+//                }
+//
+//                print(item)
+//
+//            }
+
+
     }
 
     
-    //checks in 10km radius
+    
+    //checks the radius
     func locationIsInRange(myLocation: CLLocation, surveyLocation: CLLocation) -> Bool {
     if myLocation.distance(from: surveyLocation) < locationRadius {
         return true
         }
         return false
     }
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
