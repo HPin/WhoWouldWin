d3f4587bb5f7e115d34b3f2d34c33474a2d6a8d6
ValentinWitz <s1610237037@students.fh-hagenberg.at>
2018-01-18 00:15:07 +0100

    Added possibility to add global or location based Battles

--- WhoWouldWin/AddingTab/AddViewController.swift
+++ WhoWouldWin/AddingTab/AddViewController.swift
@@ -8,14 +8,17 @@
 
 import UIKit
 import FirebaseDatabase
+import CoreLocation
 
-class AddViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+class AddViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, CLLocationManagerDelegate {
     
     var ref: DatabaseReference?
     var refHandle: DatabaseHandle?
+    let manager = CLLocationManager()
     var categories = ["Fight", "Rap Battle", "Beauty Contest"]
     var categoryClicked: String = "Fight"   // default: First picker entry
     var battleCount: UInt = 0
+    var globalOrLocation: Int = 0
     
     @IBOutlet weak var locationSegControl: UISegmentedControl!
     @IBOutlet weak var categoryPicker: UIPickerView!
@@ -23,19 +26,34 @@ class AddViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDat
     @IBOutlet weak var name2Label: UITextField!
     
     
+    
     @IBAction func saveButton(_ sender: UIBarButtonItem) {
         
-        let battleRef = ref?.child("Categories").child(categoryClicked).childByAutoId()
-        battleRef?.child("Contender 1").setValue(["Name": name1Label.text, "Votes" : 0])
-        battleRef?.child("Contender 2").setValue(["Name": name2Label.text, "Votes" : 0])
+        if globalOrLocation == 0 {
+            let battleRef = ref?.child("Categories").child(categoryClicked).childByAutoId()
+            
+            //write in the actual data
+            battleRef?.child("Contender 1").setValue(["Name": name1Label.text, "Votes" : 0])
+            battleRef?.child("Contender 2").setValue(["Name": name2Label.text, "Votes" : 0])
+        }
+        else if globalOrLocation == 1{
+            let battleRef = ref?.child("Locations").child(categoryClicked).childByAutoId()
+            manager.delegate = self
+            manager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            manager.requestWhenInUseAuthorization()
+            manager.startUpdatingLocation()
+            guard let latitude:Double = manager.location?.coordinate.latitude else {return}
+            guard let longitude:Double = manager.location?.coordinate.longitude else {return}
+            
+            //write in the actual data
+            battleRef?.setValue(["Latitude": latitude, "Longitude": longitude])
+            battleRef?.child("Contender 1").setValue(["Name": name1Label.text, "Votes": 0])
+            battleRef?.child("Contender 2").setValue(["Name": name2Label.text, "Votes": 0])
+        }
         
         
-//        let battleNo = categoryClicked + " " + String(battleCount)
-//
-//        let battleRef = ref?.child("Categories").child(categoryClicked).child(battleNo)
-//
-//        battleRef?.child("Contender 1").setValue(["Name": "Jakob", "Votes" : 0])
-//        battleRef?.child("Contender 2").setValue(["Name": "Toaster", "Votes" : 0])
+        
+        
     }
     
     override func viewDidLoad() {
@@ -49,6 +67,15 @@ class AddViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDat
        
     }
 
+    @IBAction func GlobalOrLocationBattle(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            globalOrLocation = 0
+        }
+        else{
+            globalOrLocation = 1
+        }
+    }
+    
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
     }
