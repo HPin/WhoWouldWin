d0d95276c9c1c44db566b2f40f0f45f7d284faeb
ValentinWitz <s1610237037@students.fh-hagenberg.at>
2018-01-18 20:40:42 +0100

    display location battles

--- WhoWouldWin/LocationTab/LocationBattleViewController.swift
+++ WhoWouldWin/LocationTab/LocationBattleViewController.swift
@@ -12,14 +12,67 @@ import FirebaseDatabase
 
 class LocationBattleViewController: UIViewController, CLLocationManagerDelegate {
     
-    let manager = CLLocationManager()
+    @IBOutlet weak var percentage2Label: UILabel!
+    @IBOutlet weak var contender1Label: UILabel!
+    @IBOutlet weak var contender2Label: UILabel!
+    @IBOutlet weak var percentage1Label: UILabel!
+    @IBOutlet weak var vote1Button: UIButton!
+    @IBOutlet weak var vote2Button: UIButton!
+    @IBOutlet weak var nextButton: CustomButton!
+    @IBOutlet weak var errorLabel: UILabel!
+    
     var ref: DatabaseReference?
     var loc: DatabaseReference?
     var refHandle: DatabaseHandle?
+    
+    let manager = CLLocationManager()
     var locationRadius:Double = 10
+    
     var idArr = [String]()
     var battlesArr:[[String:AnyObject]]?
     var randomIndex: Int = 0
+    var votesContender1: Int = 0
+    var votesContender2: Int = 0
+
+    
+    @IBAction func nextButton(_ sender: Any) {
+        vote1Button.isHidden = false
+        vote2Button.isHidden = false
+        nextButton.isHidden = true
+        
+        battlesArr?.remove(at: randomIndex)
+        idArr.remove(at: randomIndex)
+        displayBattle()
+    }
+    
+    @IBAction func vote1Button(_ sender: Any) {
+        votesContender1 += 1
+
+        let ID = idArr[randomIndex]
+        let battleRef = ref?.child("Locations").child(ID)
+
+        battleRef?.child("Contender 1").child("Votes").setValue(votesContender1)
+
+        self.percentage1Label.text = String(votesContender1)
+
+        vote1Button.isHidden = true
+        vote2Button.isHidden = true
+        nextButton.isHidden = false
+    }
+    @IBAction func vote2Button(_ sender: Any) {
+        votesContender2 += 1
+
+        let ID = idArr[randomIndex]
+        let battleRef = ref?.child("Locations").child(ID)
+
+        battleRef?.child("Contender 2").child("Votes").setValue(votesContender2)
+
+        self.percentage2Label.text = String(votesContender2)
+
+        vote1Button.isHidden = true
+        vote2Button.isHidden = true
+        nextButton.isHidden = false
+    }
     
     override func viewWillAppear(_ animated: Bool) {
         manager.delegate = self
@@ -36,49 +89,57 @@ class LocationBattleViewController: UIViewController, CLLocationManagerDelegate 
         
         ref = Database.database().reference()
         
-        ref?.child("Locations").observe(.childAdded, with: { (snapshot) in
+        refHandle = ref?.child("Locations").observe(.childAdded, with: { (snapshot) in
             print(snapshot)
             if let dic = snapshot.value as? [String:AnyObject] {
-                print("----------")
-                print(dic)
                 let latitude:Double = dic["Latitude"] as! Double
                 let longitude:Double = dic["Longitude"] as! Double
                 let location = CLLocation(latitude: latitude, longitude: longitude)
                 let location1 = CLLocation(latitude: myLatitude, longitude: myLongitude)
+                print("-----------------------------------")
+                print("Location: ", latitude , longitude)
+                print("My Location: ", myLatitude , myLongitude)
+                print("-----------------------------------")
                 if self.locationIsInRange(myLocation: location, surveyLocation: location1){
-                    //there is a Battle with the location range
+                    print("There is a fight in your location")
                     if self.battlesArr?.append(dic) == nil {
                         self.battlesArr = [dic]
                     }
                     self.idArr.append(snapshot.key)
                 }
             }
+            self.displayBattle()
         })
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
     }
     
     func displayBattle() {
         if let arr = battlesArr {
             let len = arr.count
             if len != 0 {
-                //errorLabel.isHidden = true
+                errorLabel.isHidden = true
                 randomIndex = Int(arc4random_uniform(UInt32(len)))
                 
                 let dict = arr[randomIndex]
-                //self.contender1Label.text = dict["Contender 1"]!["Name"] as? String
+                self.contender1Label.text = dict["Contender 1"]!["Name"] as? String
                 let votes1 = dict["Contender 1"]!["Votes"] as! Int
-                //self.percent1Label.text = String(votes1)
+                self.percentage1Label.text = String(votes1)
                 
-                //self.contender2Label.text = dict["Contender 2"]!["Name"] as? String
+                self.contender2Label.text = dict["Contender 2"]!["Name"] as? String
                 let votes2 = dict["Contender 2"]!["Votes"] as! Int
-                //self.percent2Label.text = String(votes2)
+                self.percentage2Label.text = String(votes2)
                 
-//                votesContender1 = votes1
-//                votesContender2 = votes2
+                votesContender1 = votes1
+                votesContender2 = votes2
             } else {
-//                vote1Button.isHidden = true
-//                vote2Button.isHidden = true
-//                nextButton.isHidden = true
-//                errorLabel.isHidden = false
+                vote1Button.isHidden = true
+                vote2Button.isHidden = true
+                nextButton.isHidden = true
+                errorLabel.isHidden = false
                 
             }
             
@@ -90,15 +151,11 @@ class LocationBattleViewController: UIViewController, CLLocationManagerDelegate 
     
     func locationIsInRange(myLocation: CLLocation, surveyLocation: CLLocation) -> Bool {
     if myLocation.distance(from: surveyLocation) < locationRadius*1000 {
+        print("Success Location distance is: ", myLocation.distance(from: surveyLocation))
         return true
         }
+        print("Failed Location distance is: ", myLocation.distance(from: surveyLocation))
         return false
-    }
-    
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
     }
     
 }
