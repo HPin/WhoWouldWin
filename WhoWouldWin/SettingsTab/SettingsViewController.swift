0dea52fef19bd9b220a934156f3daf969d5ad7ee
ValentinWitz <s1610237037@students.fh-hagenberg.at>
2018-01-27 14:03:06 +0100

    sc

---
+++ WhoWouldWin/SettingsTab/SettingsViewController.swift
@@ -0,0 +1,65 @@
+//
+//  SettingsViewController.swift
+//  WhoWouldWin
+//
+//  Created by Valentin Witzeneder on 27.01.18.
+//  Copyright © 2018 HPJSVW. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+import Firebase
+
+class SettingsViewController: UIViewController {
+
+    var ref: DatabaseReference?
+    var refHandle: DatabaseHandle?
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var userImage: UIImageView!
+    @IBOutlet weak var settingsTableView: UITableView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let userUID = Auth.auth().currentUser?.uid
+        usernameLabel.text = getUsernameFromUID(uid: userUID!)
+        
+    }
+    
+    
+    func getUsernameFromUID(uid: String) -> String {
+        var username = ""
+        ref = Database.database().reference()
+        ref?.child("Users").observeSingleEvent(of: .value) { (snapshot) in
+            let enumerator = snapshot.children
+            while let rest = enumerator.nextObject() as? DataSnapshot{
+                if let dic = rest.value as? [String:AnyObject]{
+                    if dic["uid"] as? String == uid{
+                        username = (dic["name"] as? String)!
+                        break
+                    }
+                }
+            }
+        }
+        return username
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
